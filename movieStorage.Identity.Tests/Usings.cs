54823b9951503bc7b371f913f73eb0702f8e4a60
cheeze <maksprofy51@gmail.com>
2023-01-09 18:32:14 +0200

    Finished unit testing for registration

--- movieStorage.Identity.Tests/Usings.cs
+++ movieStorage.Identity.Tests/Usings.cs
@@ -1,2 +1,6 @@
 global using NUnit.Framework;
 global using movieStorage.Identity.Controllers;
+global using movieStorage.Identity.Configuration;
+global using movieStorage.Identity.Exceptions;
+global using Moq;
+global using AutoMapper;
