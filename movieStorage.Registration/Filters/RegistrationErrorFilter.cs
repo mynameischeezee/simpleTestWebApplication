39bcbfc859b58d4a7ef74f172b9deda0df9bbc32
cheeze <maksprofy51@gmail.com>
2023-01-31 16:46:05 +0200

    refactoring

--- movieStorage.Registration/Filters/RegistrationErrorFilter.cs
+++ movieStorage.Registration/Filters/RegistrationErrorFilter.cs
@@ -11,35 +11,36 @@ public class RegistrationErrorFilter : ExceptionFilterAttribute
 {
     public override void OnException(ExceptionContext exceptionContext)
     {
-        if (exceptionContext.Exception is RegistrationFailedException registrationFailedException)
+        switch (exceptionContext.Exception)
         {
-            var errors = registrationFailedException.Errors;
-            Log.Error($"Failed to register user. Inputted values are not valid: {registrationFailedException.Errors}");
-            exceptionContext.Result = new JsonResult(errors)
+            case RegistrationFailedException registrationFailedException:
+            {
+                var errors = registrationFailedException.Errors;
+                Log.Error($"Failed to register user. Inputted values are not valid: {registrationFailedException.Errors}");
+                exceptionContext.Result = new JsonResult(errors)
                 {
                     StatusCode = 400
                 };
-        }
-        else if (exceptionContext.Exception is ModelInvalidException modelInvalidException)
-        {
-            Log.Error($"Failed to register user. Model is invalid: {modelInvalidException.Message}");
-            exceptionContext.Result = exceptionContext.Result = new JsonResult(modelInvalidException.Message)
-            {
-                StatusCode = 400
-            };
-        }
-        else if (exceptionContext.Exception is SqlException sqlException)
-        {
-            Log.Error($"Failed to register user. Can't produce SQL query: {sqlException.Errors}");
-            exceptionContext.Result = new JsonResult(sqlException.Errors)
-            {
-                StatusCode = 400
-            };
-        }
-        else
-        {
-            Log.Error($"Failed to register user. Exception is unhandled: {exceptionContext.Exception.Message}");
-            exceptionContext.Result = new BadRequestObjectResult(exceptionContext.Exception.Message);
+                break;
+            }
+            case ModelInvalidException modelInvalidException:
+                Log.Error($"Failed to register user. Model is invalid: {modelInvalidException.Message}");
+                exceptionContext.Result = exceptionContext.Result = new JsonResult(modelInvalidException.Message)
+                {
+                    StatusCode = 400
+                };
+                break;
+            case SqlException sqlException:
+                Log.Error($"Failed to register user. Can't produce SQL query: {sqlException.Errors}");
+                exceptionContext.Result = new JsonResult(sqlException.Errors)
+                {
+                    StatusCode = 400
+                };
+                break;
+            default:
+                Log.Error($"Failed to register user. Exception is unhandled: {exceptionContext.Exception.Message}");
+                exceptionContext.Result = new BadRequestObjectResult(exceptionContext.Exception.Message);
+                break;
         }
         base.OnException(exceptionContext);
     }
